cff891d60748210792bebf5a4b11be2563206f19
Tuğrul Sübekci <tugrulsubekci@gmail.com>
2022-05-27 14:30:59 +0300

    Add project files.

---
+++ Supernatural_Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supernatural_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Storm storm1 = new Storm("wind",false, "Zul'rajas");
+
+            Console.WriteLine(storm1.Announce());
+
+            Pupil pupil1 = new Pupil("Mezil-kree");
+
+            Storm storm2 = pupil1.CastWindStorm();
+
+            Console.WriteLine(storm2.Announce());
+
+            Mage mage1 = new Mage("Gul'dan");
+
+            Storm storm3 = mage1.CastRainStorm();
+
+            Console.WriteLine(storm3.Announce());
+
+            Archmage archmage1 = new Archmage("Nielas Aran");
+
+            Storm storm4 = archmage1.CastRainStorm();
+            Storm storm5 = archmage1.CastLightningStorm();
+
+            Console.WriteLine(storm4.Announce());
+            Console.WriteLine(storm5.Announce());
+
+
+            // to keep console open
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Supernatural_Inheritance/Storm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supernatural_Inheritance
+{
+    internal class Storm
+    {
+        public string Essence { get; private set; }
+        public bool IsStrong { get; private set; }
+        public string Caster { get; private set; }
+
+        public Storm(string essence, bool isstrong, string caster)
+        {
+            Essence = essence;
+            IsStrong = isstrong;
+            Caster = caster;
+        }
+
+        public string Announce()
+        {
+            if (IsStrong)
+            {
+                return $"{Caster} cast a strong {Essence} storm!";
+            }
+            else
+            {
+                return $"{Caster} cast a weak {Essence} storm!";
+            }
+        }
+    }
+}
